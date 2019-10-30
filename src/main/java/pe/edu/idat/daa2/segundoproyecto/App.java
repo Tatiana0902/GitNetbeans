d535712e0707a50d68903f59c45c629b50eac718
David <d.derosemberg@outlook.com>
2019-10-30 11:59:41 -0500

    + Moriras MUAJAJAJAJAJA

--- src/main/java/pe/edu/idat/daa2/segundoproyecto/App.java
+++ src/main/java/pe/edu/idat/daa2/segundoproyecto/App.java
@@ -4,6 +4,9 @@ public class App {
 
     public static void main(String[] args) {
         System.out.println("Hola GIT_02");
+        
+        System.out.println("Moriras en 7 días");
+        System.out.println("No soy sabrina xd");
     }
     
 }
